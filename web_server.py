30d4cf67dc8319a4ef2de4ba61e8c6f256557110
yormmeee <seunademoye@outlook.com>
2024-09-20 01:27:46 +0500

    create web server file

---
+++ web_server.py
@@ -0,0 +1,17 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class SimpleWebServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Respond with 200 OK status
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        
+        # Response content: "Hello, World"
+        response = "Hello, World"
+        self.wfile.write(response.encode('utf-8'))
+
+if __name__ == "__main__":
+    web_server = HTTPServer(('0.0.0.0', 8000), SimpleWebServer)
+    print("Webserver listening http://0.0.0.0:8000")
+    web_server.serve_forever()
